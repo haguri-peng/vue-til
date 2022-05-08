35d978d57e988cddaea6023666dfac042fb9f36a
haguri-peng <hty851111@naver.com>
2022-05-08 14:36:59 +0900

    작음 따옴표로 변경

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require("@vue/cli-service");
+// const path = require('path');
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       '@': path.resolve(__dirname, './src'),
+  //     },
+  //     extensions: ['.js', '.vue', '.json'],
+  //   },
+  // },
 });
