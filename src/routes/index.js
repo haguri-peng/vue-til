e0998d3a029c9138c58becf4ac4fff3693bcf275
haguri-peng <hty851111@naver.com>
2022-05-16 01:18:37 +0900

    Code Splitting & NotFound Page 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@
 // import VueRouter from 'vue-router';
 import { createWebHistory, createRouter } from 'vue-router';
 
-import LoginPage from '@/views/LoginPage.vue';
-import SignupPage from '@/views/SignupPage.vue';
+// import LoginPage from '@/views/LoginPage.vue';
+// import SignupPage from '@/views/SignupPage.vue';
 
 // Vue.use(VueRouter);
 
@@ -22,12 +22,28 @@ import SignupPage from '@/views/SignupPage.vue';
 
 const routes = [
   {
+    path: '/',
+    redirect: '/login',
+  },
+  {
     path: '/login',
-    component: LoginPage,
+    // component: LoginPage,
+    // Code Splitting (Dynamic Import)
+    component: () => import('@/views/LoginPage.vue'),
   },
   {
     path: '/signup',
-    component: SignupPage,
+    // component: SignupPage,
+    // Code Splitting (Dynamic Import)
+    component: () => import('@/views/SignupPage.vue'),
+  },
+  {
+    path: '/notfound',
+    component: () => import('@/views/NotFoundPage.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/notfound',
   },
 ];
 
