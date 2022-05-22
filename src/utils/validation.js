50069f1cd5c1bea27863180f862773fafac7ec30
haguri-peng <hty851111@naver.com>
2022-05-22 23:17:41 +0900

    add validation.js

---
+++ src/utils/validation.js
@@ -0,0 +1,10 @@
+const validateEmail = email => {
+  var matchedInfo = String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  return matchedInfo == null ? false : true;
+};
+
+export { validateEmail };
