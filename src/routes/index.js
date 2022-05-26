345e3f82ae98abe5649f139d7b5b65ee45705ffc
haguri <hty851111@naver.com>
2022-05-26 17:56:02 +0900

    add '/main' path

--- src/routes/index.js
+++ src/routes/index.js
@@ -38,6 +38,10 @@ const routes = [
     component: () => import('@/views/SignupPage.vue'),
   },
   {
+    path: '/main',
+    component: () => import('@/views/MainPage.vue'),
+  },
+  {
     path: '/notfound',
     component: () => import('@/views/NotFoundPage.vue'),
   },
