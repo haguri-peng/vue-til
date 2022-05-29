e7d7e32bd5fcd38190b92243d4313f3efac48090
haguri-peng <hty851111@naver.com>
2022-05-30 01:28:19 +0900

    post 데이터를 가져와서 보여주기. moment를 활용하여 시간값 변경.

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,22 +2,52 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <ul>
+        <li v-for="postItem in postItems" :key="postItem._id">
+          <div class="post-title">
+            {{ postItem.title }}
+          </div>
+          <div class="post-contents">
+            {{ postItem.contents }}
+          </div>
+          <div class="post-time">
+            {{ getCreatedTime(postItem.createdAt) }}
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
+import moment from 'moment-timezone';
 import { fetchPosts } from '@/api/index';
 
 export default {
+  data() {
+    return {
+      postItems: [],
+    };
+  },
   methods: {
     async fetchData() {
-      const response = await fetchPosts();
-      console.log(response);
+      const { data } = await fetchPosts();
+      console.log(data);
+
+      this.postItems = data.posts;
+    },
+    getCreatedTime(isoDate) {
+      let newDate = moment
+        .utc(isoDate)
+        .tz('Asia/Seoul')
+        .format('YYYY/MM/DD a h:mm:ss');
+      return newDate;
     },
   },
   created() {
     this.fetchData();
+
+    moment.locale('ko');
   },
 };
 </script>
