c7c50d5ee6701f918bd70fd88cc771145ba5f963
haguri-peng <hty851111@naver.com>
2022-05-29 17:01:52 +0900

    fetchPosts 추가, axios interceptor 사용

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,32 @@
 import axios from 'axios';
+import { setInterceptors } from './common/interceptors';
 
-const instance = axios.create({
-  // baseURL: 'http://localhost:3000/',
-  baseURL: process.env.VUE_APP_API_URL, // define in .env file
-});
+// axios 초기화
+function createInstance() {
+  const instance = axios.create({
+    // baseURL: 'http://localhost:3000/',
+    baseURL: process.env.VUE_APP_API_URL, // define in .env file
+  });
+  return setInterceptors(instance);
+}
 
+const instance = createInstance();
+
+// 회원가입
 function registerUser(userData) {
   // const url = 'http://localhost:3000/signup';
   // return axios.post(url, userData);
   return instance.post('signup', userData);
 }
 
+// 로그인
 function loginUser(userData) {
   return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+// 학습노트 데이터를 조회
+function fetchPosts() {
+  return instance.get('posts');
+}
+
+export { registerUser, loginUser, fetchPosts };
