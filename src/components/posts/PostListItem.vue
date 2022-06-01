26678318972a299492bd8d97a4d368f049844d5a
haguri-peng <hty851111@naver.com>
2022-06-02 02:36:56 +0900

    삭제 기능 추가

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -8,12 +8,15 @@
     </div>
     <div class="post-time">
       {{ getCreatedTime(postItem.createdAt) }}
+      <i class="icon ion-md-create"></i>
+      <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
   </li>
 </template>
 
 <script>
 import moment from 'moment-timezone';
+import { deletePost } from '@/api/posts';
 
 export default {
   props: {
@@ -29,6 +32,17 @@ export default {
         .tz('Asia/Seoul')
         .format('YYYY/MM/DD a h:mm:ss');
       return newDate;
+    },
+    async deleteItem() {
+      if (!confirm('Do you want to delete it?')) return;
+
+      try {
+        await deletePost(this.postItem._id);
+        this.$emit('refresh');
+      } catch (err) {
+        console.error(err);
+        alert(err.response.data.message);
+      }
     },
   },
   created() {
