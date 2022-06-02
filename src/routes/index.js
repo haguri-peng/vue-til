902d180fd1990e212ea06cda17f8db7fb0ac3bd6
haguri <hty851111@naver.com>
2022-06-02 17:50:27 +0900

    불필요한 주석 삭제, '/post' path 추가, Navigation guards 설정

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,5 @@
-// import Vue from 'vue';
-// import VueRouter from 'vue-router';
 import { createWebHistory, createRouter } from 'vue-router';
-
-// import LoginPage from '@/views/LoginPage.vue';
-// import SignupPage from '@/views/SignupPage.vue';
-
-// Vue.use(VueRouter);
-
-// export default new VueRouter({
-//   routes: [
-//     {
-//       path: '/login',
-//       component: LoginPage,
-//     },
-//     {
-//       path: '/signup',
-//       component: SignupPage,
-//     },
-//   ],
-// });
+import store from '@/store/index';
 
 const routes = [
   {
@@ -40,10 +21,17 @@ const routes = [
   {
     path: '/main',
     component: () => import('@/views/MainPage.vue'),
+    meta: { auth: true },
   },
   {
     path: '/add',
     component: () => import('@/views/PostAddPage.vue'),
+    meta: { auth: true },
+  },
+  {
+    path: '/post/:id',
+    component: () => import('@/views/PostEditPage.vue'),
+    meta: { auth: true },
   },
   {
     path: '/notfound',
@@ -58,6 +46,22 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Navigation guards
+// eslint-disable-next-line no-unused-vars
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    alert('인증이 필요합니다.');
+    next('/login');
+  } else {
+    // 로그인 상태에서 '/login' path가 넘어올 때에는 Main 화면으로 이동
+    if (to.path == '/login' && store.getters.isLogin) {
+      next('/main');
+    } else {
+      next();
+    }
+  }
 });
 
 export default router;
