102e75be260cc73d391839d73c76310311aa5776
haguri <hty851111@naver.com>
2022-06-02 17:45:49 +0900

    add fetchPost, editPost function

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,8 +1,13 @@
 import { posts } from './index';
 
-// 학습노트 데이터를 조회
+// 학습노트 데이터 목록 조회
 function fetchPosts() {
   return posts.get('/');
+}
+
+// 특정 학습노트 데이터를 조회
+function fetchPost(postId) {
+  return posts.get(postId);
 }
 
 // 학습노트 데이터를 생성
@@ -10,9 +15,14 @@ function createPost(postData) {
   return posts.post('/', postData);
 }
 
+// 학습노트 데이터를 수정
+function editPost(postId, postData) {
+  return posts.put(postId, postData);
+}
+
 // 학습노트 데이터를 삭제
 function deletePost(postId) {
   return posts.delete(postId);
 }
 
-export { fetchPosts, createPost, deletePost };
+export { fetchPosts, fetchPost, createPost, editPost, deletePost };
